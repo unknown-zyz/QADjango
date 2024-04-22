1b5197490b228b292470a1395e3bc7fdc245f175
unknown-zyz <805290114@qq.com>
2024-04-22 21:23:26 +0800

    update

--- DocSet/models.py
+++ DocSet/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from User.models import User
+
 
 class DocSet(models.Model):
     name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='docsets')
     is_active = models.BooleanField(default=True)
 
     def delete(self, using=None, keep_parents=False):
