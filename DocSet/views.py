1f4a608edb8872454dc2c9224cf7a37a73263307
caryz <Zhangkairui250@126.com>
2024-04-23 21:25:15 +0800

    Update views.py

--- DocSet/views.py
+++ DocSet/views.py
@@ -1,6 +1,19 @@
-from rest_framework import generics
+import json
+
+import requests
+from django_q.tasks import async_task
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 from .models import DocSet
 from .serializers import DocSetSerializer
+
+
+def docset_create_task(name):
+    url = f'http://172.16.26.4:8081/docsets/'
+    js = {"name": name}
+    res = requests.post(url, json=js)
+    print(res)
 
 
 class DocSetListCreateAPIView(generics.ListCreateAPIView):
@@ -9,6 +22,13 @@ class DocSetListCreateAPIView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return DocSet.objects.filter(is_active=True).all()
+
+    def post(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        DocSet.objects.create(name=name)
+        # 调用异步任务
+        async_task(docset_create_task, name)
+        return Response({'success': 'Create success'}, status=status.HTTP_201_CREATED)
 
 
 class DocSetDestroyAPIView(generics.DestroyAPIView):
