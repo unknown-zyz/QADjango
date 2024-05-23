4c277aa55285e22b96e88450c2a799e0237b02bc
caryz <Zhangkairui250@126.com>
2024-05-23 21:17:24 +0800

    1

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -83,16 +83,26 @@ WSGI_APPLICATION = "djangoProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#         "OPTIONS": {
+#             "timeout": 20,
+#         }
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "OPTIONS": {
-            "timeout": 20,
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python manage.py qcluster &
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 tail -f /dev/null
